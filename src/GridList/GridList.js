8785809b3b03a238b68b99b70f0eac767dd28e45
Damon Nguyen <damonnguyen92@gmail.com>
2017-08-19 17:42:05 -0700

    Update GridList.js

--- src/GridList/GridList.js
+++ src/GridList/GridList.js
@@ -39,6 +39,7 @@ function GridList(props) {
         const childRows = currentChild.props.rows || 1;
 
         return React.cloneElement(currentChild, {
+          className: itemClassName ? itemClassName: '',
           style: Object.assign(
             {
               width: `${100 / cols * childCols}%`,
